8b61d3f25349b0d62c7178c2bd210df5261eecb6
Shimul Chakraborty <shimul.ckbt@gmail.com>
2021-12-15 14:10:39 +0600

    crud table design added

--- project.php
+++ project.php
@@ -19,21 +19,80 @@ include 'connection.php';
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="welcome.css" />
     <title>Project</title>
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #customers td,
+        #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #customers tr:hover {
+            background-color: #ddd;
+        }
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
-    <header>
-        <h1 id="nav-title"><a href="welcome.php">EMS.ME</a></h1>
-        <nav>
-            <ul>
-                <li><a class="text-link" href="employee.php">Employee</a></li>
-                <li><a class="text-link" href="project.php">Project</a></li>
-                <li><a class="text-link" href="profile.php">Profile</a></li>
-                <li><a class="text-link" href="logout.php">Logout</a></li>
-            </ul>
-        </nav>
-    </header>
-    <h3>hello project</h3>
+    <div>
+        <header>
+            <h1 id="nav-title"><a href="welcome.php">EMS.ME</a></h1>
+            <nav>
+                <ul>
+                    <li><a class="text-link" href="employee.php">Employee</a></li>
+                    <li><a class="text-link" href="project.php">Project</a></li>
+                    <li><a class="text-link" href="profile.php">Profile</a></li>
+                    <li><a class="text-link" href="logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="padding-table">
+            <button>Add Project</button>
+
+            <table id="customers">
+                <tr>
+                    <th>SL</th>
+                    <th>Name</th>
+                    <th>Cost</th>
+                    <th>Start Date</th>
+                    <th>Operations</th>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                    <td><button>Edit</button><button>Delete</button></td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Berglunds snabbköp</td>
+                    <td>Christina Berglund</td>
+                    <td>Sweden</td>
+                    <td><button>Edit</button><button>Delete</button></td>
+
+                </tr>
+            </table>
+        </div>
+    </div>
+
 </body>
 
 </html>
--- welcome.php
+++ welcome.php
@@ -19,21 +19,110 @@ if (!isset($_SESSION['user_id'])) {
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="welcome.css" />
     <title>Home</title>
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #customers td,
+        #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #customers tr:hover {
+            background-color: #ddd;
+        }
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
     <!-- Navbar -->
-    <header>
-        <h1 id="nav-title"><a href="welcome.php">EMS.ME</a></h1>
-        <nav>
-            <ul>
-                <li><a href="employee.php">Employee</a></li>
-                <li><a href="project.php">Project</a></li>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </nav>
-    </header>
+    <div>
+        <header>
+            <h1 id="nav-title"><a href="welcome.php">EMS.ME</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="employee.php">Employee</a></li>
+                    <li><a href="project.php">Project</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="flex-table">
+            <div>
+                <button>Add Employee</button>
+
+                <table id="customers">
+                    <tr>
+                        <th>SL</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Operations</th>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td><button>Edit</button><button>Delete</button></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Berglunds snabbköp</td>
+                        <td>Christina Berglund</td>
+                        <td>Sweden</td>
+                        <td><button>Edit</button><button>Delete</button></td>
+
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <button>Add Project</button>
+
+                <table id="customers">
+                    <tr>
+                        <th>SL</th>
+                        <th>Name</th>
+                        <th>Cost</th>
+                        <th>Start Date</th>
+                        <th>Operations</th>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td><button>Edit</button><button>Delete</button></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Berglunds snabbköp</td>
+                        <td>Christina Berglund</td>
+                        <td>Sweden</td>
+                        <td><button>Edit</button><button>Delete</button></td>
+
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
